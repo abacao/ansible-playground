0cd97be28dedf10554cbc6ce06dd5b413ca0863b
André Bação <andre.amaral_bacao.ext@siemens.com>
2017-09-12 15:35:32 +0100

    fixed structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,13 @@ resource "aws_lightsail_instance" "lightsail-02" {
   blueprint_id        = "ubuntu_16_04"
   key_pair_name       = "${aws_lightsail_key_pair.lg_key_pair.name}"
 }
+
+# # Output inventory file for Ansible outside terraform folder
+# provisioner "local-exec" {
+#   command = "echo '[dev]\n${aws_lightsail_instance.lightsail-01.name} ansible_host=${aws_lightsail_instance.lightsail-01.public_ip_addres}' > inventory"
+# }
+#
+# # Output inventory file for Ansible outside terraform folder
+# provisioner "local-exec" {
+#   command = "echo '[dev]\n${aws_lightsail_instance.lightsail-02.name} ansible_host=${aws_lightsail_instance.lightsail-02.public_ip_addres}' > inventory"
+# }
